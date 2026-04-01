3ba2a86e3cc3fbe0298e6443eadf6bf71f47682b
D3monizer <vysmol@gmail.com>
2026-04-01 19:55:19 +0300

    style(nvim): format treesitter langs

--- .config/nvim/lua/custom/plugins/treesitter.lua
+++ .config/nvim/lua/custom/plugins/treesitter.lua
@@ -3,7 +3,20 @@ return {
         'nvim-treesitter/nvim-treesitter',
         build = ':TSUpdate',
         opts = {
-            ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'jsdoc' },
+            ensure_installed = {
+                'bash',
+                'c',
+                'diff',
+                'html',
+                'lua',
+                'luadoc',
+                'markdown',
+                'markdown_inline',
+                'query',
+                'vim',
+                'vimdoc',
+                'jsdoc'
+            },
             -- Autoinstall languages that are not installed
             auto_install = true,
             highlight = {
