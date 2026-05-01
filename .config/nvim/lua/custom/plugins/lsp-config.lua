ab3a186884131b94c7ecfceaf7ffe45cb64fdfe2
D3monizer <vysmol@gmail.com>
2026-05-01 21:24:58 +0300

    refactor(nvim): change fallback error and warn diag signs to real symbols

--- .config/nvim/lua/custom/plugins/lsp-config.lua
+++ .config/nvim/lua/custom/plugins/lsp-config.lua
@@ -122,10 +122,10 @@ return {
         underline = { severity = vim.diagnostic.severity.ERROR },
         signs = vim.g.have_nerd_font and {
           text = {
-            [vim.diagnostic.severity.ERROR] = '?? ',
-            [vim.diagnostic.severity.WARN] = '?? ',
-            [vim.diagnostic.severity.INFO] = '?? ',
-            [vim.diagnostic.severity.HINT] = '?? ',
+            [vim.diagnostic.severity.ERROR] = 'E',
+            [vim.diagnostic.severity.WARN] = 'W',
+            [vim.diagnostic.severity.INFO] = 'I',
+            [vim.diagnostic.severity.HINT] = 'H',
           },
         } or {},
         virtual_text = {
