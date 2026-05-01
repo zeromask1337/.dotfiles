ba55c334220961531b8fdd98e7b5c3bb457c2ec0
D3monizer <vysmol@gmail.com>
2026-05-01 20:27:25 +0300

    chore(nvim): delete gitsigns duplicate

--- .config/nvim/lua/custom/plugins/buffer.lua
+++ .config/nvim/lua/custom/plugins/buffer.lua
@@ -1,25 +1,4 @@
 return {
     -- Detect tabstop and shiftwidth automatically
     { 'NMAC427/guess-indent.nvim' },
-    { -- Adds git related signs to the gutter, as well as utilities for managing changes
-        'lewis6991/gitsigns.nvim',
-        opts = {
-            signs = {
-                add = { text = '┃' },
-                change = { text = '|' },
-                delete = { text = '_' },
-                topdelete = { text = '‾' },
-                changedelete = { text = '~' },
-                untracked = { text = '┆' },
-            },
-            signs_staged = {
-                add = { text = '┃' },
-                change = { text = '┃' },
-                delete = { text = '_' },
-                topdelete = { text = '‾' },
-                changedelete = { text = '~' },
-                untracked = { text = '┆' },
-            },
-        },
-    },
 }
