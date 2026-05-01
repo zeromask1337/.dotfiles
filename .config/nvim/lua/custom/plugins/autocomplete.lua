ac9472929e34639a2a8ec4747b2d927646c161e4
D3monizer <vysmol@gmail.com>
2026-05-01 20:30:18 +0300

    chore(nvim): delete luasnip dependency

--- .config/nvim/lua/custom/plugins/autocomplete.lua
+++ .config/nvim/lua/custom/plugins/autocomplete.lua
@@ -1,91 +1,63 @@
 return {
-  'xzbdmw/colorful-menu.nvim',
-  { -- Autocompletion
-    'saghen/blink.cmp',
-    event = 'VimEnter',
-    version = '1.*',
-    dependencies = {
-      -- Snippet Engine
-      {
-        'L3MON4D3/LuaSnip',
-        version = '2.*',
-        build = (function()
-          -- Build Step is needed for regex support in snippets.
-          -- This step is not supported in many windows environments.
-          -- Remove the below condition to re-enable on windows.
-          if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-            return
-          end
-          return 'make install_jsregexp'
-        end)(),
-        dependencies = {
-          -- `friendly-snippets` contains a variety of premade snippets.
-          --    See the README about individual language/framework/plugin snippets:
-          --    https://github.com/rafamadriz/friendly-snippets
-          -- {
-          --   'rafamadriz/friendly-snippets',
-          --   config = function()
-          --     require('luasnip.loaders.from_vscode').lazy_load()
-          --   end,
-          -- },
-        },
-        opts = {},
-      },
-    },
-    opts = {
-      keymap = {
-        preset = 'default',
-      },
-      appearance = {
-        nerd_font_variant = 'mono',
-      },
-      completion = {
-        ghost_text = { enabled = true },
-        documentation = { auto_show = false, auto_show_delay_ms = 500 },
-        menu = {
-          border = 'rounded',
-          draw = {
-            -- We don't need label_description now because label and label_description are already
-            -- combined together in label by colorful-menu.nvim.
-            padding = { 1, 10 },
-            columns = { { 'kind_icon' }, { 'label', gap = 1 } },
-            components = {
-              label = {
-                text = function(ctx)
-                  return require('colorful-menu').blink_components_text(ctx)
-                end,
-                highlight = function(ctx)
-                  return require('colorful-menu').blink_components_highlight(ctx)
-                end,
-              },
+    'xzbdmw/colorful-menu.nvim',
+    { -- Autocompletion
+        'saghen/blink.cmp',
+        event = 'VimEnter',
+        version = '1.*',
+        opts = {
+            keymap = {
+                preset = 'default',
             },
-            treesitter = { 'lsp' },
-          },
+            appearance = {
+                nerd_font_variant = 'mono',
+            },
+            completion = {
+                ghost_text = { enabled = true },
+                documentation = { auto_show = false, auto_show_delay_ms = 500 },
+                menu = {
+                    border = 'rounded',
+                    draw = {
+                        -- We don't need label_description now because label and label_description are already
+                        -- combined together in label by colorful-menu.nvim.
+                        padding = { 1, 10 },
+                        columns = { { 'kind_icon' }, { 'label', gap = 1 } },
+                        components = {
+                            label = {
+                                text = function(ctx)
+                                    return require('colorful-menu').blink_components_text(ctx)
+                                end,
+                                highlight = function(ctx)
+                                    return require('colorful-menu').blink_components_highlight(ctx)
+                                end,
+                            },
+                        },
+                        treesitter = { 'lsp' },
+                    },
+                },
+            },
+            sources = {
+                default = { 'lsp', 'path', 'snippets', 'lazydev', 'buffer' },
+                providers = {
+                    lazydev = { name = 'LazyDev', module = 'lazydev.integrations.blink', score_offset = 100 },
+                },
+            },
+            snippets = { preset = 'default' },
+            fuzzy = { implementation = 'prefer_rust_with_warning' },
+            signature = { enabled = true },
         },
-      },
-      sources = {
-        default = { 'lsp', 'path', 'snippets', 'lazydev', 'buffer' },
-        providers = {
-          lazydev = { name = 'LazyDev', module = 'lazydev.integrations.blink', score_offset = 100 },
-        },
-      },
-      snippets = { preset = 'default' },
-      fuzzy = { implementation = 'prefer_rust_with_warning' },
-      signature = { enabled = true },
     },
-  },
-  {
-    'windwp/nvim-ts-autotag',
-    opts = {},
-  },
-  {
-    'supermaven-inc/supermaven-nvim',
-    config = function()
-      require('supermaven-nvim').setup {
-        keymaps = {
-          accept_suggestion = '<C-l>',
-        },
-      }
-    end,
-  },
+    {
+        'windwp/nvim-ts-autotag',
+        opts = {},
+    },
+    {
+        'supermaven-inc/supermaven-nvim',
+        config = function()
+            require('supermaven-nvim').setup {
+                keymaps = {
+                    accept_suggestion = '<C-l>',
+                },
+            }
+        end,
+    },
 }
