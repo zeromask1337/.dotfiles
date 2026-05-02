7c2d0fc5a504f51e9ff9cb8ca2d501914322c4d6
D3monizer <vysmol@gmail.com>
2026-05-02 18:26:43 +0300

    refactor(nvim): make telescope window 50/50 and change layout if window is small

--- .config/nvim/lua/custom/plugins/telescope.lua
+++ .config/nvim/lua/custom/plugins/telescope.lua
@@ -26,6 +26,21 @@ return { -- Fuzzy Finder (files, lsp, etc)
   },
   config = function()
     require('telescope').setup {
+      defaults = {
+        layout_strategy = "flex",
+        layout_config = {
+          flex = {
+            flip_columns = 120,
+          },
+          horizontal = {
+            preview_width = 0.5,
+            preview_cutoff = 1,
+          },
+          vertical = {
+            preview_cutoff = 1,
+          },
+        },
+      },
       extensions = {
         ['ui-select'] = {
           require('telescope.themes').get_dropdown(),
